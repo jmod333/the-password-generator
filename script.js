5a81dff6171dc6008bd4897347082e0ff2146355
jmod333 <116365950+jmod333@users.noreply.github.com>
2023-02-10 11:32:51 -0500

    Changed lowerCase, upperCase, numbers, and specialChars strings into arrays

--- script.js
+++ script.js
@@ -1,24 +1,91 @@
-/*GIVEN I need a new, secure password
-WHEN I click the button to generate a password
-THEN I am presented with a series of prompts for password criteria
-WHEN prompted for password criteria
-THEN I select which criteria to include in the password
-WHEN prompted for the length of the password
-THEN I choose a length of at least 8 characters and no more than 128 characters
-WHEN asked for character types to include in the password
-THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
-WHEN I answer each prompt
-THEN my input should be validated and at least one character type should be selected
-WHEN all prompts are answered
-THEN a password is generated that matches the selected criteria
-WHEN the password is generated
-THEN the password is either displayed in an alert or written to the page*/
-
 // Assignment code here
-var lowerCase = "abcdefghijklmnopqrstuvwxyz";
-var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var numbers = "0123456789";
-var specialChars = "~`!@#$%^&*()_-+=[]{}|<>?/;:'";
+var lowerCase = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+var upperCase = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var specialChars = [
+  "~",
+  "`",
+  "!",
+  "@",
+  "#",
+  "$",
+  "%",
+  "^",
+  "&",
+  "*",
+  "(",
+  ")",
+  "_",
+  "-",
+  "+",
+  "=",
+  "[",
+  "]",
+  "{",
+  "}",
+  "|",
+  "<",
+  ">",
+  "?",
+  "/",
+  ";",
+  ":",
+  "'",
+];
 
 // var includeLowCase = prompt("Include lower case characters?");
 
@@ -27,6 +94,35 @@ var generateBtn = document.querySelector("#generate");
 
 // define generatePassword
 function generatePassword() {
+  // GIVEN I need a new, secure password
+  // WHEN I click the button to generate a password
+  // THEN I am presented with a series of prompts for password criteria
+  // WHEN prompted for password criteria
+  var passwordLength = prompt("How long would you like your password to be?");
+  // THEN I select which criteria to include in the password
+  // WHEN prompted for the length of the password
+  // THEN I choose a length of at least 8 characters and no more than 128 characters
+  if (passwordLength < 8 || passwordLength > 128) {
+    alert("Outside range of characters");
+    return null;
+  }
+  // WHEN asked for character types to include in the password
+  // THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
+  // WHEN I answer each prompt
+  var includeLowCase = confirm("Include lower case characters?");
+  var includeUpperCase = confirm("Include upper case characters?");
+  var includeNumbers = confirm("Include numbers?");
+  var includeSpecialChars = confirm("Include special characters?");
+  // THEN my input should be validated and at least one character type should be selected
+  // WHEN all prompts are answered
+
+  // HOW TO CHECK IF AT LEAST ONE OF THE CONFIRMS IS TRUE????
+
+  // THEN a password is generated that matches the selected criteria
+  // WHEN the password is generated
+  // THEN the password is either displayed in an alert or written to the page
+
+  // function will return the final password
   return "hello";
 }
 
